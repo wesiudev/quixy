1340b80948480b146267084628173deb312d5b55
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-11 23:18:17 +0200

    first commit

--- app/blog/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function BlogLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div className="w-full min-h-screen bg-rose-300">{children}</div>;
-}
--- app/blog/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function loading() {
-  return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-rose-300 text-white font-sans">
-      Ładowanie postów...
-    </div>
-  );
-}
