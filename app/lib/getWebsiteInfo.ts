e67219b69ee9a845b2879c1622ba9a010d1d8d3a
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-12 00:30:27 +0200

    first commit

--- app/lib/getWebsiteInfo.ts
+++ app/lib/getWebsiteInfo.ts
@@ -1,6 +1,8 @@
 "use server";
 export async function getWebsiteInfo(city: string, websiteId: string) {
-  const res = await fetch(`/api/shop/${city}/${websiteId}`);
+  const res = await fetch(
+    `http://localhost:3000/api/shop/${city}/${websiteId}`
+  );
   if (!res) {
     throw new Error("Failed to fetch data");
   }
