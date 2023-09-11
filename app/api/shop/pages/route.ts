5e70749ed06d77ad8d2da3b63f8e34f54e0c4d20
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-11 23:48:38 +0200

    first commit

--- app/api/shop/pages/route.ts
+++ app/api/shop/pages/route.ts
@@ -2,9 +2,5 @@ import { NextResponse } from "next/server";
 import ShopContent from "@/public/json/ShopContent.json";
 
 export async function GET() {
-  try {
-    return NextResponse.json({ pages: ShopContent.shopItems });
-  } catch (error) {
-    return new NextResponse("not found", { status: 500 });
-  }
+  return NextResponse.json({ pages: ShopContent.shopItems });
 }
