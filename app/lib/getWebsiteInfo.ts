fc581d15ac3490ec1492126ac25fa3cf191f9325
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-13 08:26:24 +0200

    first commit

--- app/lib/getWebsiteInfo.ts
+++ app/lib/getWebsiteInfo.ts
@@ -1,7 +1,7 @@
 "use server";
 export async function getWebsiteInfo(city: string, websiteId: string) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/shop/pages?city=${city}&service=${websiteId}&secret=${process.env.API_SECRET_KEY}`
+    `/api/shop/pages?city=${city}&service=${websiteId}&secret=${process.env.API_SECRET_KEY}`
   );
   if (!res) {
     throw new Error("Failed to fetch data");
