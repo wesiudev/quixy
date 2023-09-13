464d0cc07735c5d9ec94a55f8a40565ea39f38d8
wesselpawel <wesselpawel@users.noreply.github.com>
2023-09-13 08:33:42 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,16 @@ import UnderHero from "./components/UnderHero";
 import Shop from "./components/shop/Shop";
 import { getShopContent } from "./lib/getShopContent";
 
-async function getServicesList() {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/services`);
-  // Recommendation: handle errors
-  if (!req.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  const services = req.json();
-  return services;
-}
+// async function getServicesList() {
+//   const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/services`);
+//   // Recommendation: handle errors
+//   if (!req.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
+//   const services = req.json();
+//   return services;
+// }
 
 export default async function Home() {
   const ItemsList = await getShopContent();
@@ -21,7 +21,7 @@ export default async function Home() {
     <>
       <Hero />
       <UnderHero />
-      <Shop ItemsList={ItemsList} />
+      {/* <Shop ItemsList={ItemsList} /> */}
       <About />
     </>
   );
--- app/strony-internetowe/page.tsx
+++
@@ -1,5 +0,0 @@
-export default async function Page() {
-  return (
-    <div className="min-h-screen w-full bg-gradient-to-b from-rose-300 to-white flex flex-col items-center gap-y-3"></div>
-  );
-}
