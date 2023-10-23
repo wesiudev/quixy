ff06bd535f30755ba34c23b0d7c0945913a49b09
wesselpawel <wesselpawel@users.noreply.github.com>
2023-10-23 20:58:49 +0200

    quixy 2/3

--- app/components/HeroModule/HeroModule.tsx
+++ app/components/HeroModule/HeroModule.tsx
@@ -7,24 +7,16 @@ export default function HeroModule({ filter }: { filter?: boolean }) {
     <>
       <div className="h-[35vh] lg:h-[35vh] duration-1000 w-screen flex items-center overflow-hidden top-[50%] -translate-y-[50%] absolute flex-col lg:flex-row bg-gradient-to-b from-green-500  to-blue-500">
         {/* first plane image */}
-        <div className="absolute h-screen -left-[12vw] xl:-left-[5vw] w-auto overflow-hidden top-[50%] -translate-y-[50%] z-10 object-cover">
-          <Image
-            src="/assets/bg.webp"
-            alt="Hero Image Tworzenie stron internetowych Quixy.pl"
-            quality={50}
-            width={1920}
-            height={1080}
-          />
-        </div>
-        <div className="hidden lg:block absolute -right-[12vw] 2xl:-right-[3vw] h-screen w-auto overflow-hidden top-[50%] -translate-y-[50%] z-[5] object-cover">
-          <Image
-            src="/assets/bg2.webp"
-            alt="Hero Image Tworzenie stron internetowych Quixy.pl"
-            quality={50}
-            width={1920}
-            height={1080}
-          />
-        </div>
+        <img
+          src="/assets/bg.webp"
+          alt="Hero Image Tworzenie stron internetowych Quixy.pl"
+          className="absolute h-screen -left-[12vw] xl:-left-[5vw] w-auto overflow-hidden top-[50%] -translate-y-[50%] z-10 object-cover"
+        />
+        <img
+          src="/assets/bg2.webp"
+          alt="Hero Image Tworzenie stron internetowych Quixy.pl"
+          className="hidden lg:block absolute -right-[12vw] 2xl:-right-[3vw] h-screen w-auto overflow-hidden top-[50%] -translate-y-[50%] z-[5] object-cover"
+        />
 
         <div
           className={`flex items-center text-center justify-center text-6xl w-full h-full absolute right-0 top-0 font-coco text-white drop-shadow-2xl shadow-black z-50 ${
@@ -37,24 +29,16 @@ export default function HeroModule({ filter }: { filter?: boolean }) {
       </div>
       <div className="w-screen h-screen mt-0 bg-zinc-900 bg-opacity-75">
         {/* second plane image */}
-        <div className="absolute h-screen w-auto overflow-hidden -left-[12vw] xl:-left-[5vw] top-[50%] -translate-y-[50%] -z-[5] opacity-100 object-cover blur-md">
-          <Image
-            src="/assets/bg.webp"
-            alt="Hero Image Tworzenie stron internetowych Quixy.pl"
-            quality={50}
-            width={1920}
-            height={1080}
-          />
-        </div>
-        <div className="hidden lg:block absolute h-screen w-auto overflow-hidden top-[50%] -translate-y-[50%] -right-[12vw] 2xl:-right-[3vw] -z-10 opacity-100 object-cover blur-md">
-          <Image
-            src="/assets/bg2.webp"
-            alt="Hero Image Tworzenie stron internetowych Quixy.pl"
-            quality={50}
-            width={1920}
-            height={1080}
-          />
-        </div>
+        <img
+          src="/assets/bg.webp"
+          alt="Hero Image Tworzenie stron internetowych Quixy.pl"
+          className="absolute h-screen w-auto overflow-hidden -left-[12vw] xl:-left-[5vw] top-[50%] -translate-y-[50%] -z-[5] opacity-100 object-cover blur-md"
+        />
+        <img
+          src="/assets/bg2.webp"
+          alt="Hero Image Tworzenie stron internetowych Quixy.pl"
+          className="hidden lg:block absolute h-screen w-auto overflow-hidden top-[50%] -translate-y-[50%] -right-[12vw] 2xl:-right-[3vw] -z-10 opacity-100 object-cover blur-md"
+        />
       </div>
     </>
   );
